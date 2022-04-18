8fffb199847c2cd163e45e00b1f36f930eb2bf83
benbegins <62623777+benbegins@users.noreply.github.com>
2022-04-18 18:23:07 +0200

    Analytics tag

--- header.php
+++ header.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107977583-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-107977583-1');
+	</script>
+    
     <meta charset="<?php bloginfo("charset"); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"/>
     
